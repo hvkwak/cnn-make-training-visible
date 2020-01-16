a5e70e608989ad515c826953125a6afc93f36061
Hyovin Kwak <hyovin.kwak@tu-dortmund.de>
2020-01-16 18:48:35 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,110 +1,179 @@
+'''
+Some of the useful links: build a classifier using keras 
+https://gist.github.com/fchollet/0830affa1f7f19fd47b06d4cf89ed44d#file-classifier_from_little_data_script_1-py-L49
+Dataset is available at: 
+https://www.kaggle.com/sanikamal/rock-paper-scissors-dataset
+Please note that...:
+### : Aufgabestellung - Name
+##  : Comments
+#   : disables the code
+'''
+
 from __future__ import print_function
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.datasets import mnist
+import tensorflow.keras as keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D, Dropout
+from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras import backend as K
-import matplotlib.image
-import os
-from PIL import Image
-
+# import matplotlib.image
+# import os
+# from PIL import Image
 tf.enable_eager_execution()
 
+#################################################################
+#                  Load Data and Preprocessing                  #
+#################################################################
 
-class MyCallback(keras.callbacks.Callback):
-    def __init__(self, _model):
-        super().__init__()
-        self.model = _model
-        self.weights = []
+## Some parameters are set randomly. After the model is complete, 
+## will change it back to normal.
 
-    def on_epoch_end(self, epoch, logs=None):  # store all weights before training epoch
-        curr_layer = self.model.layers[0]
-        filters, biases = curr_layer.get_weights()
-
-        # normalize weights to 0 - 1 for visualiztion
-        w_min, w_max = filters.min(), filters.max()
-        filters = (filters - w_min) / (w_max - w_min)
-
-        for i in range(filters.shape[3]):
-            f = filters[:, :, :, i]
-            # TODO: check how many channels the filter has
-            f = np.squeeze(f)
-            dir = "filters/filter" + str(i) + "/"
-            if not os.path.isdir(dir):
-                os.makedirs(dir)
-            # matplotlib.image.imsave(dir + str(epoch) + ".png", f, cmap="gray")
-            Image.fromarray((f * 255).astype(np.uint8)).convert("L").resize((30, 30)).save(dir + str(epoch) + ".png")
-
-        # self.weights.append(filters)
-
-    # def on_epoch_end(self, epoch, logs=None): # store all weights after training epoch
-    #     curr_weights = []
-    #     curr_layer = self.model.layers[1:][0]
-    #     curr_weights.append( curr_layer.weights[0].numpy() )
-    #
-    #     weight_norms = []
-    #     for curr_weight, init_weight in zip(curr_weights, self.init_weights):
-    #         weight_norms.append( np.linalg.norm( curr_weight - init_weight ) )
-
-    #     print('\nWeight norms:' + str(weight_norms) + '\n')
-
-
-batch_size = 128
-num_classes = 10
-epochs = 30
-
-# input image dimensions
-img_rows, img_cols = 28, 28
-
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+### Get ready to load data and preprocess - Hyobin
+input_shape = (200,300,3)
+num_classes = 3
+datagen = ImageDataGenerator()
+train_data_dir = '/home/Documents/data/train' # Change for run
+validation_data_dir = '/home/Documents/data/validation'
+test_data_dir = '/home/Documents/data/test'
+nb_train_samples = 100
+nb_validation_samples = 10
+epochs = 1
+batch_size = 10
 
 if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
+    input_shape = (3, img_width, img_height)
 else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+    input_shape = (img_width, img_height, 3)
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+### Preprocess the data properly - Hyobin
+raise NotImplementedError('Implement me')
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+### convert class vectors to binary class matrices - Hyobin
+raise NotImplementedError('Implement me')
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
+
+
+
+###########################################################################
+# Implement the architecture from original paper(Zeiler and Fergus, 2014) #
+###########################################################################
+
+### Implement Layer 1 - Arslan
+model.add(Conv2D(filters=96, kernel_size=(7,7), strides=2, 
+                 activation='relu', input_shape=input_shape))
+model.add(MaxPool2D(pool_size=(3,3), strides=2))
+
+## Implement these two if needed:
+### The contrast normaliztion for layer 1(should be implemented here) - Arslan
+raise NotImplementedError('Implement me')
+
+### model.add(normalization.BatchNormalization()) - Arslan
+raise NotImplementedError('Implement me')
+
+### Implement Layer 2 - Arslan
+model.add(Conv2D(filters=256, kernel_size=(7,7), strides=2, 
+                 activation='relu')
+model.add(MaxPool2D(pool_size=(3,3), strides=2))
+
+
+
+### Implement Layer 3 - Aya
+raise NotImplementedError('Implement me')
+### Implement Layer 4 - Aya
+raise NotImplementedError('Implement me')
+### Implement Layer 5 - Aya
+raise NotImplementedError('Implement me')
+### Implement Layer 6 - Aya
+raise NotImplementedError('Implement me')
+
+
+
+### Implement Layer 7 and summary() - Dimitri
 model.add(Dense(num_classes, activation='softmax'))
+model.summary()
+### Implement model.compile() - Dimitri
+raise NotImplementedError('Implement me')
+### Implement train_datagen = ImageDataGenerator() - Dimitri
+raise NotImplementedError('Implement me')
 
-model.compile(loss=keras.losses.categorical_crossentropy,
+
+
+
+
+## Implement rest of ImageDataGenerators and fit_generator
+### Implement test_datagen = ImageDataGenerator() - Simon
+raise NotImplementedError('Implement me')
+### Implement train_generator = train_datagen.flow_from_directory() - Simon
+raise NotImplementedError('Implement me')
+### Implement test_generator = test_datagen.flow_from_directory() - Simon
+raise NotImplementedError('Implement me')
+### Implement model.fit_generator() - Simon
+raise NotImplementedError('Implement me')
+
+
+
+
+
+
+
+
+
+
+
+# We will not use the alternatives, won't we? Let us forget about these:
+# Alternative models suggested by Arslan
+model2 = Sequential()
+model2.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=(200,300,3)))
+model2.add(Conv2D(64, (3, 3), activation='relu'))
+model2.add(MaxPooling2D(pool_size=(2, 2)))
+model2.add(Dropout(0.25))
+model2.add(Flatten())
+model2.add(Dense(128, activation='relu'))
+model2.add(Dropout(0.5))
+model2.add(Dense(num_classes, activation='softmax'))
+
+model2.compile(loss=categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test),
-          callbacks=[MyCallback(model)])
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+model2.summary()
+
+#################################
+### Another alternative model ###
+#################################
+
+model3 = Sequential()
+# Step 1 - Convolution
+model3.add(Conv2D(32, (3, 3), padding='same', input_shape = input_shape, activation = 'relu'))
+model3.add(Conv2D(32, (3, 3), activation='relu'))
+model3.add(MaxPooling2D(pool_size=(2, 2)))
+model3.add(Dropout(0.5)) # antes era 0.25
+
+# Adding a second convolutional layer
+model3.add(Conv2D(64, (3, 3), padding='same', activation = 'relu'))
+model3.add(Conv2D(64, (3, 3), activation='relu'))
+model3.add(MaxPooling2D(pool_size=(2, 2)))
+model3.add(Dropout(0.5)) # antes era 0.25
+
+# Adding a third convolutional layer
+model3.add(Conv2D(64, (3, 3), padding='same', activation = 'relu'))
+model3.add(Conv2D(64, (3, 3), activation='relu'))
+model3.add(MaxPooling2D(pool_size=(2, 2)))
+model3.add(Dropout(0.5)) # antes era 0.25
+
+# Step 3 - Flattening
+model3.add(Flatten())
+# Step 4 - Full connection
+model3.add(Dense(units = 512, activation = 'relu'))
+model3.add(Dropout(0.5)) 
+model3.add(Dense(units = num_classes, activation = 'softmax'))
+
+# Compiling the CNN
+model3.compile(optimizer = 'rmsprop',
+                   loss = 'categorical_crossentropy', 
+                   metrics = ['accuracy'])
